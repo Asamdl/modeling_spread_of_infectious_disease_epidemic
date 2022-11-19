3b2bf78e847c2db65d3d83437fbfdce5b8bd9ea1
Asamdl <titans_ana>
2022-11-19 10:54:24 +0500

    feat(interface): [6] add класс WidgetCreatingModel, fix class Window

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
-from PyQt5 import QtGui
+from PyQt5 import QtGui, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, \
-    QLineEdit, QRadioButton, QStackedWidget, QGridLayout, QProgressBar, QSlider
+    QLineEdit, QRadioButton, QStackedWidget, QGridLayout, QProgressBar, QSlider, QCheckBox, QFrame
 import sys
 from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
 from PyQt5.QtCore import Qt, QObject, QEvent
@@ -10,86 +10,30 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import random
 import matplotlib.pyplot as plt
 
-class Widget_Plt(QWidget):
+
+class WidgetPlt(QWidget):
     def __init__(self):
         super().__init__()
         self.show()
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self, coordinates=False)#I've been looking for you for too long to just leave you here unmarked
+        # I've been looking for you for too long to just leave you here unmarked
+        self.toolbar = NavigationToolbar(self.canvas, self,
+                                         coordinates=False)
         self.button = QPushButton('Plot')
         self.button.clicked.connect(self.plot)
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
         layout.addWidget(self.button)
-
         self.setLayout(layout)
 
-    # action called by the push button
     def plot(self):
         data = [random.random() for i in range(10)]
         self.figure.clear()
         ax = self.figure.add_subplot(111)
         ax.plot(data, '*-')
         self.canvas.draw()
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        loadUi("interface.ui", self)
-        self.title = "PyQt5 Drawing Tutorial"
-        self.top = 150
-        self.left = 150
-        self.width = 500
-        self.height = 500
-        self.label2.setText("text")
-        self.InitWindow()
-
-    def InitWindow(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.top, self.left, self.width, self.height)
-        self.show()
-
-
-class OverLay(QWidget):
-    def __init__(self, *args, **kwargs):
-        QWidget.__init__(self, *args, **kwargs)
-        self.setAttribute(Qt.WA_NoSystemBackground)
-        self.setAttribute(Qt.WA_TransparentForMouseEvents)
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.fillRect(self.rect(), QColor(80, 80, 255, 128))
-
-
-class Filter(QObject):
-    def __init__(self, *args, **kwargs):
-        QObject.__init__(self, *args, **kwargs)
-        self.m_overlay = None
-        self.m_overlayOn = None
-
-    def eventFilter(self, obj, event):
-        if not obj.isWidgetType():
-            return False
-        if event.type() == QEvent.MouseButtonPress:
-            if not self.m_overlay:
-                self.m_overlay = OverLay(obj.parentWidget())
-            self.m_overlay.setGeometry(obj.geometry())
-            self.m_overlayOn = obj
-            self.m_overlay.show()
-        elif event.type() == QEvent.Resize:
-            if self.m_overlay and self.m_overlayOn == obj:
-                self.m_overlay.setGeometry(obj.geometry())
-        return False
-
-
-class LocationParameter():
-    def __init__(self, x_axis, y_axis, margin_x_from_el, margin_y_from_el):
-        self.x_axis = 0.1
-        self.y_axis = 0.1
-        self.margin_x_from_el = 0.1
-        self.margin_y_from_el = 0.1
 
 
 class box(QWidget):
@@ -112,81 +56,56 @@ class box(QWidget):
     def paintEvent(self, event=None, changing_number=None):
         print(event)
         painter = QPainter(self)
-        painter.setPen(QPen(Qt.green, 8, Qt.SolidLine))
-        painter.setBrush(QBrush(Qt.red, Qt.CrossPattern))
+        painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.green, Qt.CrossPattern))
         w = self.geometry().width()
         h = self.geometry().height()
-        #params = self.get_parameters_for_rectangles()
-        #for param in params:
         painter.drawRect(0, 0, w, h)
-
-    def get_parameters_for_rectangles(self):
-        w = self.geometry().width()
-        h = self.geometry().height()
-        result = []
-        data = self.zone_location_parameters
-        coef = 1 / self.number_of_zones
-        for i in range(self.number_of_zones):
-            if i != self.number_of_zones - 1:
-                result.append(
-                    [int(w * data.x_axis / 100), int(h * data.y_axis / 100), int(w * (100 - 2 * data.x_axis) / 100),
-                     int(h * (100 - 2 * data.y_axis) / 100)])
-            else:
-                result.append(
-                    [int(coef * w * data.x_axis),
-                     int(coef)]
-                )
-        return result
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.title = "PyQt5"
-        self.top = 150
-        self.left = 150
-        self.width = 500
-        self.height = 500
-
-
-
+        self.top = 50
+        self.left = 50
+        self.width = 1000
+        self.height = 650
         grid = QGridLayout()
-        #grid.setRowStretch(10,10)
-        #grid.setColumnStretch(10,10)
         self.setLayout(grid)
-
-        names = ['Cls', 'Bck',
-                 'Cls', 'Bck']
-
-        positions = [(i, j) for i in range(2) for j in range(2)]
-        for position, name in zip(positions, names):
+        positions = [
+            (0, 0, 1, 2),
+            (0, 2, 1, 1),
+            (1, 0, 1, 1),
+            (1, 1, 1, 1),
+            (1, 2, 1, 1)
+        ]
+        # row: int, column: int, rowSpan: int, columnSpan: int
+        for position in positions:
             if position[0] == 0 and position[1] == 0:
                 buttons_lay = QHBoxLayout()
                 button_zone_increment = QPushButton("+")
-                #button_zone_increment.clicked.connect(self.add_zone)
                 buttons_lay.addWidget(button_zone_increment)
                 buttons_lay.addWidget(QPushButton("-"))
                 lay00 = QVBoxLayout()
                 lay00.addLayout(buttons_lay)
-                #grid.addLayout(buttons_lay,*position)
-                # widget = box()
-                #grid.addWidget(widget, *position)
                 lay_ = QGridLayout()
                 for x in range(2):
                     for y in range(2):
                         widget = box()
                         lay_.addWidget(widget, x, y)
                 lay00.addLayout(lay_)
+                slider = QSlider(Qt.Horizontal)
+                lay00.addWidget(slider)
                 grid.addLayout(lay00, *position)
-            elif position[0] == 0 and position[1] == 1:
-                plotlib = Widget_Plt()
+            elif position[0] == 0 and position[1] == 2:
+                plotlib = WidgetPlt()
                 lay_ = QVBoxLayout()
                 lay_.addWidget(plotlib)
                 grid.addLayout(lay_, *position)
-            elif position[0] == 1 and position[1] == 1:
-                plotlib = Widget_Plt()
+            elif position[0] == 1 and position[1] == 0:
                 lay_ = QVBoxLayout()
-                lay_.addWidget(plotlib)
+                lay_.addWidget(WidgetCreatingModel())
                 grid.addLayout(lay_, *position)
             else:
                 # label = QLabel(name)
@@ -198,25 +117,6 @@ class Window(QWidget):
     def InitWindow(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
-        self.show()
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
-
-
-class WidgetZone(QWidget):
-    def __init__(self, name=None,parent=None):
-        QWidget.__init__(self, parent=parent)
-        lay = QVBoxLayout(self)
-        self.q_widget_zone = box()
-        self.name = name
-        lay.addWidget(self.q_widget_zone)
-
-    def add_zone(self):
-        self.q_widget_zone.paintEvent(f"{self.name}")
 
 
 class Widget1(QWidget):
@@ -227,59 +127,63 @@ class Widget1(QWidget):
             lay.addWidget(QPushButton("{}".format(i)))
 
 
-class Widget2(QWidget):
+class WidgetCreatingModel(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent=parent)
-        lay = QVBoxLayout(self)
-        for i in range(4):
-            lay.addWidget(QLineEdit("{}".format(i)))
+        #self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
+        #self.setStyleSheet("border: 1px solid black;")
+
+        self.stages_names = ["S", "I", "E", "R", "D", "`S"]
+        self.name_of_inactive_stages = ["S", "I"]
+        self.checkbox_states = dict()
+        self.stages_widges = dict()
+        ley = QVBoxLayout(self)
+        label_widget = QLabel("Сreating a model")
+        ley.addWidget(label_widget)
+        for stage_name in self.stages_names:
+            widget_check_box = QCheckBox(stage_name, self)
+            widget_check_box.stateChanged.connect(self.update_info_about_status_of_checkboxes)
+            ley.addWidget(widget_check_box)
+            if stage_name in self.name_of_inactive_stages:
+                widget_check_box.setEnabled(False)
+                widget_check_box.toggle()
+                self.checkbox_states[stage_name] = True
+            else:
+                self.checkbox_states[stage_name] = False
+
+            self.stages_widges[stage_name] = widget_check_box
+        self.btn = QPushButton("apply")
+        self.btn.clicked.connect(self.show_info_about_status_of_checkboxes)
+        ley.setAlignment(Qt.AlignCenter)
 
 
-class Widget3(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent=parent)
-        lay = QVBoxLayout(self)
-        for i in range(4):
-            lay.addWidget(QRadioButton("{}".format(i)))
+        #self.btn.setStyleSheet("color:white")
+        #self.btn.setStyleSheet("background-color:green")
+
+        # frame = QFrame(self)
+        # frame.setFrameShape(QFrame.StyledPanel)
+        # frame.setLineWidth((float)(0.6))
+        #
+        # ley.addWidget(frame)
+        ley.addWidget(self.btn)
 
 
-class stackedExample(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent=parent)
-        lay = QVBoxLayout(self)
-        self.Stack = QStackedWidget()
-        self.Stack.addWidget(Widget1())
-        self.Stack.addWidget(Widget2())
-        self.Stack.addWidget(Widget3())
+    def update_info_about_status_of_checkboxes(self, state):
+        for stage_name, stage_widget in self.stages_widges.items():
+            self.checkbox_states[stage_name] = True if stage_widget.checkState() == Qt.Checked else False
 
-        btnNext = QPushButton("Next")
-        btnNext.clicked.connect(self.onNext)
-        btnPrevious = QPushButton("Previous")
-        btnPrevious.clicked.connect(self.onPrevious)
-        btnLayout = QHBoxLayout()
-        btnLayout.addWidget(btnPrevious)
-        btnLayout.addWidget(btnNext)
+    def show_info_about_status_of_checkboxes(self):
+        for name, state in self.checkbox_states.items():
+            print(f"{name} = {state}")
+        print()
 
-        lay.addWidget(self.Stack)
-        lay.addLayout(btnLayout)
 
-    def onNext(self):
-        self.Stack.setCurrentIndex((self.Stack.currentIndex() + 1) % 3)
-
-    def onPrevious(self):
-        self.Stack.setCurrentIndex((self.Stack.currentIndex() - 1) % 3)
+def main():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    # app = QApplication(sys.argv)
-    # filt = Filter()
-    # window = QWidget()
-    # lay = QVBoxLayout(window)
-    # for text in ("Foo", "Bar", "Baz"):
-    #     label = QLabel(text)
-    #     lay.addWidget(label)
-    #     label.installEventFilter(filt)
-    # window.setMinimumSize(500, 500)
-    # window.show()
-    # sys.exit(app.exec_())
     main()
