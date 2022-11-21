0854c9c95a358475c35a03eb960b3d09f1217173
Asamdl <Asamdl@yandex.ru>
2022-11-21 16:10:21 +0500

    feat(WZoneConstructor): [14] fixed adding and removing zones

--- Widgets/WZone.py
+++ Widgets/WZone.py
@@ -3,14 +3,14 @@ from Widgets.WPainter import WPainter
 import uuid
 
 
-class Zone(QWidget):
+class WZone(QWidget):
     def __init__(self, name):
         super().__init__()
         self.lay = QVBoxLayout(self)
         lay_ = QHBoxLayout()
-        self.name = name
+        self.name = str(name)
         self.id = str(uuid.uuid4())
-        self.label_name = QLabel(str(self.name))
+        self.label_name = QLabel(self.name)
         lay_.addWidget(self.label_name)
         self.btn_del = QPushButton("del")
         lay_.addWidget(self.btn_del)
@@ -18,5 +18,5 @@ class Zone(QWidget):
         self.lay.addWidget(WPainter())
 
     def set_name(self, name):
-        self.name = name
-        self.label_name.setText(str(name))
+        self.name = str(name)
+        self.label_name.setText(self.name)
