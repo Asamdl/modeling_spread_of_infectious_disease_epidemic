5db68fb8be3ff63a979934b5580f5ce97fcf25bd
Asamdl <Asamdl@yandex.ru>
2022-11-21 15:41:54 +0500

    feat(WZone): [13] add id field for zone

--- Widgets/WZone.py
+++ Widgets/WZone.py
@@ -1,9 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, \
-    QLineEdit, QRadioButton, QStackedWidget, QGridLayout, QProgressBar, QSlider, QCheckBox, QFrame
-from PyQt5.QtCore import Qt, QObject, QEvent
-from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
-
+from PyQt5.QtWidgets import QLabel, QWidget, QHBoxLayout, QVBoxLayout, QPushButton
 from Widgets.WPainter import WPainter
+import uuid
 
 
 class Zone(QWidget):
@@ -12,6 +9,7 @@ class Zone(QWidget):
         self.lay = QVBoxLayout(self)
         lay_ = QHBoxLayout()
         self.name = name
+        self.id = str(uuid.uuid4())
         self.label_name = QLabel(str(self.name))
         lay_.addWidget(self.label_name)
         self.btn_del = QPushButton("del")
