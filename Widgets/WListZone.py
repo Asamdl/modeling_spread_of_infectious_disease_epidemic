49c205cf4edc991f15f37ef5481992fcad5937c1
Asamdl <Asamdl@yandex.ru>
2022-12-04 13:36:08 +0500

    feat(wlistzone): [21] latest version of wlistzone

--- Widgets/WListZone.py
+++ Widgets/WListZone.py
@@ -8,33 +8,61 @@ from functools import partial
 from PyQt5.QtCore import Qt, QObject, QEvent
 
 from Widgets.WZoneElement import WZoneElement
+from PyQt5 import QtCore, QtWidgets
 
 
 class WListZone(QWidget):
+
     def __init__(self):
         super().__init__()
-        self.number_of_zones = 3
+        self.number_of_zones = 0
+        self.zones_names = dict()
+        self.window_layout = QVBoxLayout(self)
+        layout_add_zone = QHBoxLayout()
+        self.input_name = QLineEdit()
+        self.input_name.setPlaceholderText('Название зоны')
+        self.btn = QtWidgets.QPushButton("Доавить")
+        self.btn.clicked.connect(self.add_item)
+        layout_add_zone.addWidget(self.input_name)
+        layout_add_zone.addWidget(self.btn)
+        self.window_layout.addLayout(layout_add_zone)
+        self.listWidget = QListWidget()
+        # self.listWidget.addItem(QListWidgetItem("Solo"))
+        self.listWidget.installEventFilter(self)
+        # self.listWidget.itemSelectionChanged.connect(self.selectionChanged)
+        self.listWidget.itemActivated.connect(self.itemActivated_event)
 
-        self.zones = dict()
-        self.layout = QVBoxLayout(self)
-        self.layout.addStretch()
-        self.widget = QWidget()
-        self.vbox = QVBoxLayout(self.widget)
+        # listWidgetItem = QListWidgetItem("GeeksForGeeks")
+        # self.listWidget.addItem(listWidgetItem)
 
-        for i in range(3):
-            w = QWidget()
-            hbox = QHBoxLayout(w)
-            hbox.addWidget(WZoneElement(str(i)))
+        self.window_layout.addWidget(self.listWidget)
 
-            self.vbox.addWidget(w)
+    def selectionChanged(self):
+        el = self.listWidget.selectedItems()
+        print("Selected items: ", self.listWidget.selectedItems())
 
-        self.scroll = QScrollArea()
-        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.scroll.setWidgetResizable(True)
-        self.scroll.setWidget(self.widget)
+    def update_list_widget(self):
+        self.listWidget.clear()
+        for zone_name in self.zones_names.keys():
+            self.listWidget.addItem(QListWidgetItem(zone_name))
 
-        self.layout.addWidget(self.scroll)
-        self.layout.addStretch()
+    def itemActivated_event(self, item):
+        print(item.text())
 
+    def eventFilter(self, obj, event):
+        if obj is self.listWidget and event.type() == QtCore.QEvent.ContextMenu:
+            item = self.listWidget.currentItem()
+            if item:
+                name = item.text()
+                del self.zones_names[name]
+                self.update_list_widget()
+                print(f'element {name} del')
+        return super().eventFilter(obj, event)
 
+    def add_item(self):
+        name = f"{self.input_name.text()}"
+        if name not in self.zones_names:
+            self.zones_names[name] = "zod"
+            self.listWidget.addItem(QListWidgetItem(name))
+        else:
+            self.input_name.clear()
