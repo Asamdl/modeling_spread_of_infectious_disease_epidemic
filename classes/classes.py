edaae475f829e7fdf14f3dd8c3768977fbbdffb7
Asamdl <Asamdl@yandex.ru>
2022-12-17 14:09:33 +0500

    feat(classes): [44] add class DiseaseStage, DFactor, OWFlow. add methods get_flow, get_change

--- classes/classes.py
+++ classes/classes.py
@@ -30,10 +30,55 @@ class Settings:
         return data
 
 
+def input_to_num(string):
+    if isinstance(string, str):
+        rez_string = string.strip()
+        rez_string = rez_string.replace(",", ".")
+        if "." in rez_string:
+            return float(rez_string)
+        else:
+            return int(rez_string)
+
+
 class Stage:
     def __init__(self, name: str = None, start_num: str = None):
         self.name = name if name is not None else ""
         self.start_num = start_num if start_num is not None else ""
+
+    class DiseaseStage:
+        def __init__(self, name, num):
+            self.name = name
+            self.num = num
+
+        def __repr__(self):
+            return "DiseaseStage: {0} ({1})".format(self.name, self.num)
+
+    def get_stage(self, st_i):
+        start_num = None
+        info = []
+        check = True
+        DS = None
+        try:
+            start_num = input_to_num(self.start_num)
+            if self.name == "":
+                info.append(["Empty_stage_name", st_i, ""])
+                check = False
+            if start_num < 0:
+                info.append(["Negative_start_num", st_i, ""])
+                check = False
+            if isinstance(start_num, float):
+                info.append(["Float_start_num", st_i, ""])
+                check = False
+
+        except ValueError:
+            info.append(["Incorrect_entered_value", st_i, "Stage"])
+            check = False
+        except Exception as e:
+            raise SystemExit(1)
+        if check:
+            DS = self.DiseaseStage(self.name, start_num)
+
+        return [DS, check, info]
 
 
 class Flow:
@@ -60,10 +105,68 @@ class Flow:
             data[dict_stage.stage_name] = dict_stage.value
         return data
 
+    def get_flow(self, model_state, population_size, step, divided_n):
+        """
+        Calculate the flow value given the state of the model
+        :param model_state: dictionary
+        :return: int
+        """
+        flow = model_state[self.source]
+        if self.induction:
+            ind_flow = 0
+            ind_flow += float(self.dic_ind[0].value) * model_state["I"]
+            flow *= ind_flow
+            if divided_n:
+                flow /= population_size
+        flow *= self.s_factor
+        return flow
+
+    def get_change(self, model_state, population_size, step, divided_n):
+        """
+        Returns the change value given the state of the model
+        :param model_state: dictionary, {name: num}
+        :return: dictionary, {stage: flow}
+        """
+
+        flow = self.get_flow(model_state, population_size, step, divided_n)
+        changes = {self.source: -flow}
+        changes[self.dic_target[0].stage_name] = flow * float(self.dic_target[0].value)
+        return [changes, flow]
+
+    def __repr__(self):
+        present = "Flow: ({f}) {s} -> ".format(f=str(self.s_factor), s=self.source)
+
+        present += self.dic_target[0].stage_name + ", "
+        if self.induction:
+            present += "\ninducing by: "
+            present += self.dic_ind[0].stage_name + ", "
+        else:
+            present += "\n not inducing"
+
+        return present
+
+
+class DFactor:
+    def __init__(self, name, dic_values=dict()):
+        self.name = name
+        self.dic_values = dic_values
+
+
+class OWFlow:
+    def __init__(self, stage="", s_factor="1", d_factor="",
+                 dynamic=False, direction=True, relativity="stage"):
+        self.stage = stage
+        self.s_factor = s_factor
+        self.d_factor = d_factor
+        self.dynamic = dynamic
+        self.direction = direction
+        self.relativity = relativity
+
 
 class Model:
     def __init__(self, model_name: str = None, description: str = None, stages: list[Stage] = None,
-                 flows: list[Flow] = None, ow_flows: list = None, d_factors: list = None, settings: Settings = None,
+                 flows: list[Flow] = None, ow_flows: list[DFactor] = None, d_factors: list = None,
+                 settings: Settings = None,
                  associated_result_files: str = None):
         self.model_name = model_name if model_name is not None else ""
         self.description = description if description is not None else ""
@@ -91,8 +194,8 @@ class Model:
             for flow in self.flows:
                 data = dict()
                 data["source"] = flow.source
-                data["sfactor"] = flow.s_factor
-                data["dfactor"] = flow.d_factor
+                data["s_factor"] = flow.s_factor
+                data["d_factor"] = flow.d_factor
                 data["dynamic"] = flow.dynamic
                 data["dic_target"] = flow.get_dict_target()
                 data["induction"] = flow.induction
